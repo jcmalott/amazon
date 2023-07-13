78ba7aa8d5648fc762c6c3f7cedfa98556b058ed
malottjc <malottjc@gmail.com>
2023-07-13 13:58:57 -0700

    react app created

---
+++ frontend/src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <a href="/">Amazon</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+};
+
+export default App;
